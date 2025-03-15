72caf00eef3584ac56ed0b44cc66428a2104a81f
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-03-15 13:23:38 -0400

    added summarization model

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -14,3 +14,14 @@ logger = logging.getLogger("fundamental_analysis_agent")
 logger.setLevel(logging.INFO)
 
 app = FastAPI()
+
+# Summarization model
+model_name = "t5-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
+
+class FundamentalRequest(BaseModel):
+    ticker: str
+    year: Optional[int] = 2024
+
